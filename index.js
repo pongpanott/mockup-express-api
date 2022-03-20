47166633751c54fc6c74c8de49854dd56368cf42
pongpanott <fhyptr@gmail.com>
2022-03-20 15:16:36 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) => {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+module.exports = app;
